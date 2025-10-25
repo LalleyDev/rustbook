b79070b96b195e3e921899bf7d33f1c0073e4d6b
lalleyDev <samuellalley@gmail.com>
2025-10-25 09:27:25 -0700

    Ordering, Match and cmp
    This commit introduces the match expression. Here we are able to pass
    the arms located inside the {} to the match function. the match function
    takes the ouput of .cmp's evaluation of guess and secret_number and
    determines which println to run. This is very much like a java switch
    statement.
    
    Note: this code will not compile and run as secret_number and guess are
    two differet types. The next commit will solve this problem.

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
@@ -15,4 +16,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
